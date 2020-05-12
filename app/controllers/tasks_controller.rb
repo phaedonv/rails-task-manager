fe794712be6d05ff3bb81973e90a6fd98a5f6949
Phaedon Valasiadis <faival@protonmail.com>
2020-05-12 18:05:19 +0300

    still errors need 2 be fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,5 +6,32 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+    redirect_to tasks_path(@tasks)
+  end
+
+  # first find the task, then edit it..
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path(@tasks)
+  end
+
+  #let's add some security
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name)
   end
 end
